93798411cc1cc95267197b393a82569c74ca61f5
EXsunshine <105021057@live.asia.edu.tw>
2016-09-26 15:41:57 +0800

    ex fin

---
+++ src/ex/ex02_105021057.java
@@ -0,0 +1,32 @@
+package ex;
+/*
+ * Topic: 有一圓形，直徑為200，且中心座標為(0,0)。
+ * 	請寫一支程式可以輸入「點」的座標，並判斷「點」是否在圓形的範圍內。
+ * 	如果「點」的位置剛好在邊界的話也算是在圓形範圍內(例：x=100，y=0)。
+ * Date: 2016/09/26
+ * Author: 105021057 張岳埕
+ */
+
+import java.util.Scanner;
+
+public class ex02_105021057 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		double X = scn.nextInt();
+		double Y = scn.nextInt();
+		double m = Math.pow(X, 2) + Math.pow(Y, 2);
+		if (X > 100 & Y > 100) {
+			System.out.println(false);
+		}
+		if (X < -100 & Y < -100) {
+			System.out.println(false);
+		} else {
+			m = Math.sqrt(m);
+		}
+		System.out.println(m);
+
+	}
+
+}
--- src/ex/ex03_105021057.java
+++ src/ex/ex03_105021057.java
@@ -4,14 +4,28 @@ package ex;
  * 	每半小時40元，超過4小時以上的部份，每半小時60元，未滿半小時部分不計費。
  * 	如果您從早上10點23分停到下午3點20分，請撰寫程式計算共需繳交的停車費。
  * Date: 2016/09/26
- * Author: 1050210XX 周永振老師
+ * Author: 105021057 張岳埕
  */
-
-public class ex03_1050210XX {
+import java.util.*;
+public class ex03_105021057 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+Scanner scn = new Scanner(System.in);
+int m = scn.nextInt();
+if(m<120){
+	m = (m/30)*30;
+	System.out.println(m);
+}else{
+	if(m<240){
+		m= (m/30)*40;
+		System.out.println(m);
+				}else{
+					m = (m/30)*40;
+					System.out.println(m);
+				}
+}
 
 	}
-
+	
 }
